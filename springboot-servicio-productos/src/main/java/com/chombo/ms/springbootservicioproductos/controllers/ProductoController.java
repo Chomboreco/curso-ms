16d1f9b77a989e066b8fa58251cc9a1e7488989a
Eduardo Chombo <eddy_wallace@hotmail.com>
2021-12-13 08:16:10 -0600

    Imprimir el puerto del servicio al que llama para visualizar el balanceo de cargas con Ribbon

--- springboot-servicio-productos/src/main/java/com/chombo/ms/springbootservicioproductos/controllers/ProductoController.java
+++ springboot-servicio-productos/src/main/java/com/chombo/ms/springbootservicioproductos/controllers/ProductoController.java
@@ -1,6 +1,7 @@
 package com.chombo.ms.springbootservicioproductos.controllers;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.chombo.ms.springbootservicioproductos.models.entity.Producto;
 import com.chombo.ms.springbootservicioproductos.models.service.IProductoService;
@@ -8,6 +9,7 @@ import com.chombo.ms.springbootservicioproductos.models.service.IProductoService
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,15 +22,23 @@ public class ProductoController {
     @Autowired
     private IProductoService productoService;
 
+    @Autowired
+    private Environment env;
+
     @GetMapping("/listar")
     public List<Producto> listar() {
         log.info("Consiming /listar");
-        return productoService.findAll();
+        return productoService.findAll().stream().map(m -> {
+            m.setPort(Integer.parseInt(env.getProperty("server.port")));
+            return m;
+        }).collect(Collectors.toList());
     }
 
     @GetMapping("/ver/{id}")
     public Producto detalle(@PathVariable Long id) {
         log.info("Consiming /ver/" + id);
-        return productoService.findById(id);
+        Producto o = productoService.findById(id);
+        o.setPort(Integer.parseInt(env.getProperty("server.port")));
+        return o;
     }
 }
