e85d5f63f2d338f9abc5f291f20405ddbf63bb9b
Eduardo Chombo <eddy_wallace@hotmail.com>
2021-12-12 23:09:39 -0600

    Balanceador de cargas Ribbon - Se hace un downgrade de spring boot y cloud, para poder realizar al actividad - Utiliza Ribbon para el balanceo de cargas y se configuran para los puertos 8001 y 9001 en el application properties - Usar comando: mvn spring-boot:run -Dspring-boot.run.arguments="--server.port=9001" para sobreescribir el puerto configurado.

--- springboot-servicio-productos/src/main/java/com/chombo/ms/springbootservicioproductos/models/entity/Producto.java
+++ springboot-servicio-productos/src/main/java/com/chombo/ms/springbootservicioproductos/models/entity/Producto.java
@@ -12,11 +12,6 @@ import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 @Entity
 @Table(name = "productos")
 public class Producto implements Serializable {
@@ -34,4 +29,36 @@ public class Producto implements Serializable {
     @Column(name = "create_at")
     @Temporal(TemporalType.DATE)
     private Date createAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
 }
