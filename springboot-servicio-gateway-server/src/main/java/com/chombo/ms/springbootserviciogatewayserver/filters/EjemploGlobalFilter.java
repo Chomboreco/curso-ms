d07851ee6304543157e253e2e2a9039d3cc812e0
Eduardo Chombo <eddy_wallace@hotmail.com>
2021-12-20 22:59:33 -0600

    Modificando el Reuqest para pasar headers del request als reponse.

--- springboot-servicio-gateway-server/src/main/java/com/chombo/ms/springbootserviciogatewayserver/filters/EjemploGlobalFilter.java
+++ springboot-servicio-gateway-server/src/main/java/com/chombo/ms/springbootserviciogatewayserver/filters/EjemploGlobalFilter.java
@@ -1,9 +1,12 @@
 package com.chombo.ms.springbootserviciogatewayserver.filters;
 
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
@@ -12,18 +15,30 @@ import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 @Component
-public class EjemploGlobalFilter implements GlobalFilter {
+public class EjemploGlobalFilter implements GlobalFilter, Ordered {
 
     private final Logger log = LoggerFactory.getLogger(getClass());
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         log.info("Ejecutando pre-filtro");
-        
+        exchange.getRequest().mutate().headers(h -> h.add("token", "123456"));
+
         return chain.filter(exchange).then(Mono.fromRunnable(() -> {
             log.info("Ejecutando post-filtro");
+
+            Optional.ofNullable(exchange.getRequest().getHeaders().getFirst("token")).ifPresent(
+                    valor -> {
+                        exchange.getResponse().getHeaders().add("token", valor);
+                    });
+
             exchange.getResponse().getCookies().add("color", ResponseCookie.from("color", "rojo").build());
             exchange.getResponse().getHeaders().setContentType(MediaType.TEXT_PLAIN);
         }));
+    }
+
+    @Override
+    public int getOrder() {
+        return 1;
     }
 }
