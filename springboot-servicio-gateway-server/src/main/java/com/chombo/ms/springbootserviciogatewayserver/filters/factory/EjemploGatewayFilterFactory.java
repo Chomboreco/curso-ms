c02586b8138df5a9307e75fd82aa0ff72e9f1d62
Eduardo Chombo <eddy_wallace@hotmail.com>
2022-01-21 21:25:52 -0600

    Backup

--- springboot-servicio-gateway-server/src/main/java/com/chombo/ms/springbootserviciogatewayserver/filters/factory/EjemploGatewayFilterFactory.java
+++ springboot-servicio-gateway-server/src/main/java/com/chombo/ms/springbootserviciogatewayserver/filters/factory/EjemploGatewayFilterFactory.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
-import org.springframework.cloud.gateway.filter.OrderedGatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
@@ -24,7 +23,7 @@ public class EjemploGatewayFilterFactory
 
     @Override
     public GatewayFilter apply(Configuracion config) {
-        return new OrderedGatewayFilter((exchange, chain) -> {
+        return (exchange, chain) -> {
             log.info("Ejecutando pre gateway filter factory: " + config.mensaje);
             return chain.filter(exchange).then(Mono.fromRunnable(() -> {
                 Optional.ofNullable(config.cookieValor).ifPresent(cookie -> {
@@ -33,7 +32,7 @@ public class EjemploGatewayFilterFactory
 
                 log.info("Ejecutando post gateway filter factory");
             }));
-        }, 2);
+        };
     }
 
     @Override
