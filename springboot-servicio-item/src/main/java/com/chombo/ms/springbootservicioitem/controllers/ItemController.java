baa6e34a20914efa659924a0236e02ddc522c018
Eduardo Chombo <eddy_wallace@hotmail.com>
2022-02-01 00:28:53 -0600

    Filtros Gateway Factory que vienen de fábrica

--- springboot-servicio-item/src/main/java/com/chombo/ms/springbootservicioitem/controllers/ItemController.java
+++ springboot-servicio-item/src/main/java/com/chombo/ms/springbootservicioitem/controllers/ItemController.java
@@ -7,21 +7,29 @@ import com.chombo.ms.springbootservicioitem.model.Producto;
 import com.chombo.ms.springbootservicioitem.model.service.ItemService;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ItemController {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
 
     @Autowired
     @Qualifier("itemServiceFeign")
     private ItemService itemService;
 
     @GetMapping("/listar")
-    public List<Item> listar() {
+    public List<Item> listar(@RequestParam("nombre") String nombre, @RequestHeader("token-request") String token) {
+        log.info("nombre: " + nombre);
+        log.info("token: " + token);
         return itemService.findAll();
     }
 
