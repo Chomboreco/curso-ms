2a18a6a299a16a334fbafc329464d858598ca448
Eduardo Chombo <eddy_wallace@hotmail.com>
2022-03-07 17:24:18 -0600

    Customización de @RestResource

--- springboot-servicio-usuarios/src/main/java/com/chombo/ms/springbootserviciousuarios/models/entity/dao/UsuarioDao.java
+++ springboot-servicio-usuarios/src/main/java/com/chombo/ms/springbootserviciousuarios/models/entity/dao/UsuarioDao.java
@@ -4,12 +4,18 @@ import com.chombo.ms.springbootserviciousuarios.models.entity.Usuario;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 @RepositoryRestResource(path = "usuarios")
 public interface UsuarioDao extends PagingAndSortingRepository<Usuario, Long> {
 
-    public Usuario findByUsername(String username);
+    /*
+     * Si en un método no se le especifíca el path en @RestResource entonces tomará el nombre del método.
+     */
+    @RestResource(path = "buscar-username")
+    public Usuario findByUsername(@Param("username") String username);
 
     @Query("select u from Usuario u where u.username = ?1")
     public Usuario obtenerPorUsername(String username);
